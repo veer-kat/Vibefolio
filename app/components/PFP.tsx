8d322f3fef435e58b4027ce5e05927e9883e5d73
veer-kat <veerpkatrodia@gmail.com>
2025-10-27 13:27:16 +0530

    PFP edit button removed

--- app/components/PFP.tsx
+++ app/components/PFP.tsx
@@ -146,10 +146,10 @@ export default function EditableProfilePicture() {
         </div>
 
         <motion.button
-          className="absolute -bottom-2 -right-2 bg-gradient-to-r from-[#cc7722] to-[#9A4D00] text-white rounded-full p-2 shadow-md hover:shadow-lg transition-all"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          onClick={() => setShowEdit(true)}
+          // className="absolute -bottom-2 -right-2 bg-gradient-to-r from-[#cc7722] to-[#9A4D00] text-white rounded-full p-2 shadow-md hover:shadow-lg transition-all"
+          // whileHover={{ scale: 1.1 }}
+          // whileTap={{ scale: 0.9 }}
+          // onClick={() => setShowEdit(true)}
         >
           {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
